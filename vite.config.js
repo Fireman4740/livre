19fe9cab94197ac02b3985f56185e5bf547477c7
Fireman4740 <mathis.carlesso47@gmail.com>
2026-01-22 02:45:44 +0100

    Format JSON and Vite configuration files for consistency

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,15 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 export default defineConfig({
-  plugins: [svelte()],
-  base: '/livre/',
-  server: {
-    middlewareMode: false,
-    hmr: {
-      protocol: 'ws',
-      host: 'localhost',
-      port: 5173
-    }
-  }
-})
+	plugins: [svelte()],
+	base: "/livre/",
+	server: {
+		middlewareMode: false,
+		hmr: {
+			protocol: "ws",
+			host: "localhost",
+			port: 5173,
+		},
+	},
+});
